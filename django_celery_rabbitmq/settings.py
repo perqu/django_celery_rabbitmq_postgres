02f1d56ab5a9d67cdc48b2845e2d9e066d6906b1
Paweł Perenc <57562561+perqu@users.noreply.github.com>
2024-04-05 20:40:21 +0200

    add python-dotenv

--- django_celery_rabbitmq/settings.py
+++ django_celery_rabbitmq/settings.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q0mnti9nd71$sf#t_62d-$i@6=ht5-ljah19_-!uw)4m0vjgr&'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ['*']
 
