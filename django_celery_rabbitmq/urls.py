a894e501556190753c95d66982cd8488da15cd91
Paweł Perenc <57562561+perqu@users.noreply.github.com>
2024-04-06 12:44:56 +0200

    add health view

--- django_celery_rabbitmq/urls.py
+++ django_celery_rabbitmq/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from django_celery_rabbitmq.views import health_check, view1, view2, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('button1/', views.view1, name='view1'),
-    path('button2', views.view2, name='view2'),
+    path('', index, name='index'),
+    path('button1/', view1, name='view1'),
+    path('button2', view2, name='view2'),
+    path('health/', health_check, name='health_check'),
 ]
